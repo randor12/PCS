93fe74f139e518c1114289b79f3e6e6b2430b8cd
randor12 <rynicholas@vt.edu>
2020-02-06 18:02:17 -0500

    edited data

--- src/datastructures/ProjectRunner.java
+++ src/datastructures/ProjectRunner.java
@@ -3,6 +3,9 @@
  */
 package datastructures;
 
+import java.io.File;
+import java.util.*;
+
 /**
  * @author randor
  *
@@ -14,6 +17,24 @@ public class ProjectRunner {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		File f = new File("data.txt");
+		Scanner input;
+		try {
+			input = new Scanner(f);
+			int numInputs = input.nextInt();
+			boolean[] variables = new boolean[numInputs];
+			// Loop through to initialize all the variables
+			for (int i = 0; i < numInputs; i++) {
+				variables[i] = input.next().equals("T") ? true : false;
+			}
+			String equation = "";
+			while (input.hasNext()) {
+				equation += input.next();
+			}
+
+		} catch (Exception e) {
+			System.out.println("File not found exception");
+		}
 
 	}
 
