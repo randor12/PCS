eec46aadac51938c5cd46d1b8fae3b7a722b0c4e
randor12 <rynicholas@vt.edu>
2020-02-06 17:42:46 -0500

    Initialized project

---
+++ src/datastructures/ProjectRunner.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package datastructures;
+
+/**
+ * @author randor
+ *
+ */
+public class ProjectRunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
