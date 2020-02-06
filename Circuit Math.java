74c654d783a4a1d7d89b0d1cbf9458e3c7b0ebe3
Abhishek Sathiabalan <asathiabalan@gmail.com>
2020-02-06 17:54:04 -0500

    Update Circuit Math.java

--- Circuit Math.java
+++ Circuit Math.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class CircuitMath {
     public static void main(String[] args) {
-
+        String input = "4\nT F T F\nA B * C D + - +"; //Figure out how input works
+        //We first read the second line and store it into a Map<String, bool>
+        //Then we create a Stack<bool> and do operators like that 
     }
 }
